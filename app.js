c1530c4c4cdc573f8ec5024e46b787b33be65503
martindrapeau <martindrapeau@gmail.com>
2019-01-18 08:39:15 -0500

    .

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 $(document).ready(function() {
 
+
+	var $message = $('p.message');
+	var timeoutId;
+	function showMessage(message, reset) {
+		if (timeoutId) clearTimeout(timeoutId);
+		$message.html(message);
+		if (reset) {
+			timeoutId = setTimeout(function() {
+				$message.html('Paste (Ctrl+v) your image');
+				timeoutId = undefined;
+			}, 2000);
+		}
+	}
+
 	var IMAGE_MIME_REGEX = /^image\/(p?jpeg|gif|png)$/i;
 	var $img = $('img.image');
-	var $message = $('p.message');
-
 	function loadImage(file) {
-		$message.html('Please wait...');
+		showMessage('Please wait...');
 		var reader = new FileReader();
 		reader.onload = function(e) {
 			$img.attr('src', e.target.result);
-			$message.html('Image pasted. Paste (Ctrl+v) another to replace.');
+			showMessage('Image pasted. You can paste again to replace.');
 		};
 		reader.readAsDataURL(file);
 	};
@@ -23,6 +35,7 @@ $(document).ready(function() {
 				loadImage(items[i].getAsFile());
 				return;
 			}
+			showMessage('No image found on your Clipboard!', true);
 		}
 	});
 
