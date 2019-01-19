6f25ddd177387bb4c1b06fb1eff1bced8497d723
martindrapeau <martindrapeau@gmail.com>
2019-01-18 20:36:14 -0500

    Copy to canvas for processing

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 
-	var $message = $('p.message');
+	var $content = $('.content');
+
+	var $message = $('.message');
 	var timeoutId;
 	function showMessage(message, reset) {
 		if (timeoutId) clearTimeout(timeoutId);
@@ -13,31 +15,55 @@ $(document).ready(function() {
 		}
 	}
 
-	var IMAGE_MIME_REGEX = /^image\/(p?jpeg|gif|png)$/i;
 	var $img = $('img.image');
 	function loadImage(file) {
-		showMessage('Please wait...');
+		var deferred = new $.Deferred();
 		var reader = new FileReader();
+
+		showMessage('Loading image...');
 		reader.onload = function(e) {
 			$img.attr('src', e.target.result);
+			$canvas.hide();
+			$content.addClass('has-image');
 			showMessage('Image pasted. You can paste again to replace.');
+			setTimeout(function() {
+				deferred.resolve();
+			}, 25);
 		};
 		reader.readAsDataURL(file);
+		return deferred;
 	};
 
+	var $canvas = $('canvas');
+	function copyImageInCanvas() {
+		var canvas = $canvas[0];
+		var ctx = canvas.getContext("2d");
+		var image = $img[0];
+
+		showMessage('Copying to canvas...');
+		canvas.width = image.naturalWidth;
+		canvas.height = image.naturalHeight;
+		ctx.drawImage(image, 0, 0);
+		$img.hide();
+		$canvas.show();
+		showMessage('Image pasted. You can paste again to replace.');
+	}
+
+	var IMAGE_MIME_REGEX = /^image\/(p?jpeg|gif|png)$/i;
 	$(document).on('paste', function(e) {
     if (!e.originalEvent.clipboardData || !e.originalEvent.clipboardData.items) return;
 
 		var items = e.originalEvent.clipboardData.items;
 		for (var i = 0; i < items.length; i++) {
 			if (IMAGE_MIME_REGEX.test(items[i].type)) {
-				loadImage(items[i].getAsFile());
+				loadImage(items[i].getAsFile()).done(copyImageInCanvas);
 				return;
 			}
 			showMessage('No image found on your Clipboard!', true);
 		}
 	});
 
+	$message.html('Copy your screen by pressing on the Print Screen key. Then paste (Ctrl+v) it here as an image.');
 
 	// Inspired from https://gist.github.com/dusanmarsa/2ca9f1df36e14864328a2bb0b353332e
 });
